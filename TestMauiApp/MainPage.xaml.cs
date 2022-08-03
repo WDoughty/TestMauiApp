f5e154b619b4d61f5ade99f4625961088f8dc007
Will Doughty <wdoughty@vermeer.com>
2022-08-03 11:15:07 -0500

    Add project files.

---
+++ TestMauiApp/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace TestMauiApp;
+
+public partial class MainPage : ContentPage
+{
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+}
+
---
+++ TestMauiApp/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
+
+namespace TestMauiApp
+{
+    public partial class MainViewModel : ObservableObject
+    {
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(FullName))]
+        private string firstName;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(FullName))]
+        private string lastName;
+
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        [ObservableProperty]
+        private bool isEnabled = true;
+
+        [RelayCommand]
+        public void Submit()
+        {
+            Debug.WriteLine("Here");
+        }
+    }
+}
